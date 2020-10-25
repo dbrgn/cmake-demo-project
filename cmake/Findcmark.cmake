c8a4b6e5cc010ce5780aa6aaf903091661149019
Danilo Bargen <mail@dbrgn.ch>
2020-10-25 23:14:51 +0100

    Use set_property for setting cmark props

--- cmake/Findcmark.cmake
+++ cmake/Findcmark.cmake
@@ -1,12 +1,12 @@
 set(CMARK_SUBMODULE_BASEPATH "${PROJECT_SOURCE_DIR}/3rdparty/cmark")
 
-# Only build static lib
-set(CMARK_STATIC ON)
-set(CMARK_SHARED OFF)
-set(CMARK_TESTS OFF)
-
 # Include local submodule
 add_subdirectory("${CMARK_SUBMODULE_BASEPATH}" EXCLUDE_FROM_ALL)
+
+# Only build static lib
+set_property(TARGET libcmark_static PROPERTY CMARK_STATIC ON)
+set_property(TARGET libcmark_static PROPERTY CMARK_SHARED OFF)
+set_property(TARGET libcmark_static PROPERTY CMARK_TESTS OFF)
 
 # Alias static lib to namespaced variant
 add_library(cmark::cmark ALIAS libcmark_static)
